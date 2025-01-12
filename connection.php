389c25fe02feb9bb80a5c40b61f41fd43a8fab91
Tayyaba <81028088+Tayyaba009@users.noreply.github.com>
2025-01-12 23:41:52 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,10 +1,10 @@
 <?php 
     include_once 'apis/MysqliDb.php';
-$db = new MysqliDb('videodb.mysql.database.azure.com','afnad','Afnad123@,'assignment1');
+$db = new MysqliDb('videodb.mysql.database.azure.com','afnad','Afnad123@,'new_assignment');
  
 
 // $db = mysqli_init();
-//ysqli_real_connect($db, "videodb.mysql.database.azure.com'", "afnad", "Afnad123@", "assignment1", 3306);
+//ysqli_real_connect($db, "videodb.mysql.database.azure.com'", "afnad", "Afnad123@", "new_assignment", 3306);
    var_dump($db);
     return 0;
 ?>
